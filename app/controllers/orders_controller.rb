91e811a599850cc524fa51016a9396745ef90918
hossamkhalil01 <hossamkhalil01@gmail.clm>
2021-03-22 01:32:19 +0200

    Feature(order-page): added order status functionality

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,18 +1,27 @@
 class OrdersController < ApplicationController
     
     def index
-        @orders = current_user.orders.all();
+        @orders = current_user.orders.all
     end 
 
     def show 
-        @order = current_user.orders.find(params[:id]);
-        @order_details = @order.details.all;
+        @order = current_user.orders.find(params[:id])
+        @order_details = @order.details.all
     end 
 
-    def edit
-    end
+    def update_status
 
-    def new
-    end
+        @order = Order.find(params[:order_id])
 
+        # Check for Authorization
+        if (current_user.id  == @order.owner_id)
+        
+            @order.status = params[:status]
+            @order.save
+        
+        end
+
+        redirect_to orders_path
+    end 
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   get 'home/index'
   devise_for :users,  controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   resources :home
+
   resources :orders do
+    get '/:status', to: 'orders#update_status', as: 'update_status'
     resources :details
   end 
 
