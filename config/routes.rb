427b58ccc97dbe17386608e90ee36ea0b65201c2
Abobakr <abobakryousre@gmail.com>
2021-03-23 15:21:07 +0200

    feat(add-members-to-group): creating groups

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   devise_for :users,  controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   resources :home
   resources :users
+  resources :groups
   resources :orders do
     get '/:status', to: 'orders#update_status', as: 'update_status'
     resources :details
